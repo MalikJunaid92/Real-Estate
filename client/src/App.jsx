b45a8ad5ee9ce2b72edd0473dd13b8820c2efe11
Muhammad Junaid Raza <junaidnaveed030000@gmail.com>
2024-02-03 20:59:28 +0500

    create pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
+import Home from './pages/Home'
+import Profile from './pages/Profile'
+import About from './pages/About'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import { BrowserRouter ,Routes,Route} from 'react-router-dom'
 
 function App() {
   return (
-    <div className='text-red-500'>
-      App
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' exact element={ <Home /> } />
+      <Route path='/profile' exact element={ <Profile /> } />
+      <Route path='/sign-in' exact element={ <SignIn /> } />
+      <Route path='/sign-up' exact element={ <SignUp /> } />
+      <Route path='/about' exact element={ <About /> } />
+    </Routes>
+    </BrowserRouter>
   )
 }
 
