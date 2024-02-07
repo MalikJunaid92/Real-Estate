eb942639ff54ec09fee74825706b91de4a010fa5
Muhammad Junaid Raza <junaidnaveed030000@gmail.com>
2024-02-07 17:47:52 +0500

    create signup routes

--- api/server.js
+++ api/server.js
@@ -1,6 +1,23 @@
-import express from 'express'
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import router from "./routes/userRouter.js";
+import authrouter from "./routes/authRouter.js";
+dotenv.config();
 
+mongoose
+  .connect(process.env.MongoDb)
+  .then(() => {
+    console.log("MongoDb successfully connected");
+  })
+  .catch((err) => {
+    console.error("MongoDb connection error:", err);
+  });
+const app = express();
+app.use(express.json())
+app.listen(3000, () => {
+  console.log("Server started on port 3000");
+});
+app.use("/api/user", router);
+app.use("/api/user", authrouter);
 
-const app=express()
-
-app.listen(3000,()=>{console.log('server starting on port 3000!!')})
