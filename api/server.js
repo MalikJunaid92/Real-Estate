d97963ec234246486763eaed31d51daf7d0eecc3
Muhammad Junaid Raza <junaidnaveed030000@gmail.com>
2024-02-09 02:28:02 +0500

    create contollers

--- api/server.js
+++ api/server.js
@@ -20,4 +20,15 @@ app.listen(3000, () => {
 });
 app.use("/api/user", router);
 app.use("/api/user", authrouter);
+app.use((error, req, res, next) => {
+
+  const statusCode = error.statusCode || 500;
+  const message = error.message || 'Internal Server Error';
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message
+  });
+
+})
 
