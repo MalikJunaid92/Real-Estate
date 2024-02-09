161f4c016fb4e5b63e8e21a60b223eeadd818cfc
Muhammad Junaid Raza <junaidnaveed030000@gmail.com>
2024-02-10 02:50:02 +0500

    updated signup page

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,7 +1,10 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link,useNavigate } from "react-router-dom";
 function SignUp() {
   const [formData, setFormData] = useState({});
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate=useNavigate();
   const handleChange = (e) => {
     setFormData({
       ...formData,
@@ -11,16 +14,31 @@ function SignUp() {
   console.log(formData);
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const res = await fetch("/api/user/signup", {
-      method:'POST',
-        headers:{
-          'Content-Type':'application/json'
+    try {
+      const res = await fetch("/api/auth/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
         },
-        body:JSON.stringify(formData)
-    });
-    const data= await res.json();
-    console.log(data)
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        setLoading(false);
+        setError(data.message);
+        return;
+      }
+      setLoading(false);
+      setError(null);
+      navigate('/sign-in')
+      console.log(data); // Move console.log inside try block
+    } 
+    catch (error) {
+      setLoading(false);
+      setError(error.message);
+    }
   };
+  
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
@@ -46,8 +64,11 @@ function SignUp() {
           id="password"
           onChange={handleChange}
         />
-        <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
-          Sign in
+        <button
+          disabled={loading}
+          className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
+        >
+          {loading ? "Loading..." : "Sign Up"}
         </button>
       </form>
       <div className="flex flex-row gap-2 mt-5">
@@ -56,6 +77,7 @@ function SignUp() {
           <span className="text-blue-700">Sign In</span>
         </Link>
       </div>
+      {error && <p className="text-red-500 mt-5 ">{error}</p>}
     </div>
   );
 }
