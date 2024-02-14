10defda78ea569e78104efd397bc38eceddc6912
Muhammad Junaid Raza <junaidnaveed030000@gmail.com>
2024-02-15 02:31:54 +0500

    add pree installiation

--- api/routes/listingRoute.js
+++ api/routes/listingRoute.js
@@ -4,7 +4,7 @@ import { verifyToken } from "../utilis/verfiyUser.js";
 
 const  listingrouter = express.Router();
 
-listingrouter.get("/create",verifyToken ,createListing);
+listingrouter.post("/create",verifyToken ,createListing);
 listingrouter.delete("/delete/:id",verifyToken ,deleteListing);
 listingrouter.post("/update/:id",verifyToken ,updateListing);
 listingrouter.get("/get/:id",getListing);
--- api/server.js
+++ api/server.js
@@ -5,6 +5,7 @@ import router from "./routes/userRouter.js";
 import authrouter from "./routes/authRouter.js";
 import cookieParser from 'cookie-parser'
 import listingrouter from "./routes/listingRoute.js";
+import path from 'path'
 dotenv.config();
 
 mongoose
@@ -15,6 +16,8 @@ mongoose
   .catch((err) => {
     console.error("MongoDb connection error:", err);
   });
+
+  const __dirname=path.resolve()
 const app = express();
 app.use(express.json())
 app.use(cookieParser())
@@ -24,6 +27,11 @@ app.listen(3000, () => {
 app.use("/api/user", router);
 app.use("/api/auth", authrouter);
 app.use("/api/listing", listingrouter);
+
+app.use(express.static(path.join(__dirname,'/client/dist')))
+app.get('*',(req,res)=>{
+  res.sendFile(path.join(__dirname,'client','dist','index.html'))
+})
 app.use((error, req, res, next) => {
 
   const statusCode = error.statusCode || 500;
