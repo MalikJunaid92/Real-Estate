988f6f2f5ccbbbb8fe9ab5372fcd356bb349ab3d
Muhammad Junaid Raza <junaidnaveed030000@gmail.com>
2024-02-14 09:44:08 +0500

    create update listing api route

--- api/controllers/userControllers.js
+++ api/controllers/userControllers.js
@@ -1,7 +1,7 @@
 import { errorhandler } from "../utilis/error.js";
 import bycryptjs from "bcryptjs";
 import User from "../models/userModal.js";
-
+import Listing from '../models/listingModel.js'
 export const test = (req, res) => {
   res.json({ message: "Api route is working" });
 };
@@ -24,7 +24,7 @@ export const updateUser = async (req, res, next) => {
           avatar: req.body.avatar,
         },
       },
-      { new: true },
+      { new: true }
     );
 
     const { password, ...rest } = updatedUser._doc;
@@ -35,15 +35,43 @@ export const updateUser = async (req, res, next) => {
   }
 };
 
-
-export const deleteUser= async(req,res,next)=>{
+export const deleteUser = async (req, res, next) => {
   if (req.user.id !== req.params.id)
-  return next(errorhandler(401, 'You can only delete your own account!'));
-try {
-  await User.findByIdAndDelete(req.params.id);
-  res.clearCookie('access_token');
-  res.status(200).json('User has been deleted!');
-} catch (error) {
-  next(error);
-}
-}
+    return next(errorhandler(401, "You can only delete your own account!"));
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.clearCookie("access_token");
+    res.status(200).json("User has been deleted!");
+  } catch (error) {
+    next(error);
+  }
+};
+
+
+export const getUserListings = async (req, res, next) => {
+  if (req.user.id === req.params.id) {
+    try {
+      const listings = await Listing.find({ userRef: req.params.id });
+      res.status(200).json(listings);
+    } catch (error) {
+      next(error);
+    }
+  } else {
+    return next(errorhandler(401, 'You can only view your own listings!'));
+  }
+};
+
+export const getUser = async (req, res, next) => {
+  try {
+    
+    const user = await User.findById(req.params.id);
+  
+    if (!user) return next(errorhandler(404, 'User not found!'));
+  
+    const { password: pass, ...rest } = user._doc;
+  
+    res.status(200).json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
--- api/routes/listingRoute.js
+++ api/routes/listingRoute.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { createListing } from "../controllers/createListing.js";
+import { createListing, deleteListing, updateListing } from "../controllers/createListing.js";
 import { verifyToken } from "../utilis/verfiyUser.js";
 
-const listingrouter = express.Router();
+const  listingrouter = express.Router();
 
 listingrouter.get("/create",verifyToken ,createListing);
+listingrouter.delete("/delete/:id",verifyToken ,deleteListing);
+listingrouter.post("/update/:id",verifyToken ,updateListing);
 
 export default listingrouter;
