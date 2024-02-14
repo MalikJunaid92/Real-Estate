8958669d79fed669b010f50e2591082cc7e6946d
Muhammad Junaid Raza <junaidnaveed030000@gmail.com>
2024-02-14 11:12:14 +0500

    Add contact landlord functionaility to the listing page

--- api/routes/listingRoute.js
+++ api/routes/listingRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createListing, deleteListing, updateListing } from "../controllers/createListing.js";
+import { createListing, deleteListing, getListing, updateListing } from "../controllers/createListing.js";
 import { verifyToken } from "../utilis/verfiyUser.js";
 
 const  listingrouter = express.Router();
@@ -7,5 +7,6 @@ const  listingrouter = express.Router();
 listingrouter.get("/create",verifyToken ,createListing);
 listingrouter.delete("/delete/:id",verifyToken ,deleteListing);
 listingrouter.post("/update/:id",verifyToken ,updateListing);
+listingrouter.get("/get/:id",getListing);
 
 export default listingrouter;
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,8 @@ import { BrowserRouter ,Routes,Route} from 'react-router-dom'
 import Header from './components/Header'
 import PrivateRoute from './components/PrivateRoute'
 import CreateListing from './pages/CreateListing'
+import UpdateListing from './pages/UpdateListing'
+import Listing from './pages/Listing'
 
 function App() {
   return (
@@ -18,9 +20,11 @@ function App() {
       <Route path='/sign-in' exact element={ <SignIn /> } />
       <Route path='/sign-up' exact element={ <SignUp /> } />
       <Route path='/about' exact element={ <About /> } />
+      <Route path='/listing/:listingId' exact element={ <Listing /> } />
     <Route  element= {<PrivateRoute />}>
          <Route path='/profile' exact element={ <Profile /> } />
          <Route path='/create-listing' exact element={ <CreateListing/> } />
+         <Route path='/update-listing/:listingId' exact element={ <UpdateListing/> } />
     </Route>
     </Routes>
     </BrowserRouter>
